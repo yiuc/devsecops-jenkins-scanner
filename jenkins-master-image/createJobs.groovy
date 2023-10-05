200aa3d1b482d421bb2c2d41242bb4acff6036c4
Andy <yiuc88@gmail.com>
2023-10-05 11:52:37 +0800

    fix

--- jenkins-master-image/createJobs.groovy
+++ jenkins-master-image/createJobs.groovy
@@ -31,7 +31,7 @@ pipelineJob('AWS CodeBuild webgoat') {
 pipelineJob('pipelineJob') {
     definition {
         cps {
-            script(readFileFromWorkspace('jenkins-master-image/Jenkinsfile/pipelineJob.groovy'))
+            script(readFileFromWorkspace('jenkins-master-image/Jenkinsfile/pipelineJob'))
             sandbox()
         }
     }
