fadebd3c3fd619d838414dea774da9139178371f
Andy <yiuc88@gmail.com>
2023-10-06 16:40:18 +0800

    fix

--- gauntlt/execute_gauntlt.sh
+++ gauntlt/execute_gauntlt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -s -o /dev/null -L -v http://webgoat.svc.test.local:8080/WebGoat@reallyslow
+curl -s -o /dev/null -L -v http://webgoat.svc.test.local:8080/WebGoat
 gauntlt --tags @webgoat
 gauntlt --tags @reallyslow
 #gauntlt --tags @final
