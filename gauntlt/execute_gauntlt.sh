9f5e0c36f6dcf882209343db576a17fd83710f57
Andy <yiuc88@gmail.com>
2023-10-06 16:39:35 +0800

    gauntlt example

--- gauntlt/execute_gauntlt.sh
+++ gauntlt/execute_gauntlt.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-curl -iL http://webgoat.svc.test.local:8080/WebGoat 
+curl -s -o /dev/null -L -v http://webgoat.svc.test.local:8080/WebGoat@reallyslow
 gauntlt --tags @webgoat
+gauntlt --tags @reallyslow
 #gauntlt --tags @final
