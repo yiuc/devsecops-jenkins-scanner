980ed0673f1d56aa7a4c5bf476b7c1a2d69b3e07
Andy <yiuc88@gmail.com>
2023-10-06 14:11:15 +0800

    format

--- cdk-jenkins/app.py
+++ cdk-jenkins/app.py
@@ -1,6 +1,7 @@
 from jenkins.jenkins import JenkinsMasterStack
 from network_stack.network_stack import NetworkStack
 from codebuild_stack.codebuild import CodeBuildStack
+from webgoat_ecs_stack.ecs_stack import ECSstack
 from aws_cdk import App, Stack
 import aws_cdk as cdk
 import os
@@ -27,6 +28,16 @@ jenkins_stack = JenkinsMasterStack(
 codebuild_stack = CodeBuildStack(
     app,
     "CodeBuildStack",
+    vpc=network_stack.vpc,
+    env=cdk.Environment(
+        account=os.environ.get("CDK_DEPLOY_ACCOUNT", os.environ["CDK_DEFAULT_ACCOUNT"]),
+        region=os.environ.get("CDK_DEPLOY_REGION", os.environ["CDK_DEFAULT_REGION"]),
+    ),
+)
+
+webgoat_stack = ECSstack(
+    app,
+    "WebGoatStack",
     vpc=network_stack.vpc,
     env=cdk.Environment(
         account=os.environ.get("CDK_DEPLOY_ACCOUNT", os.environ["CDK_DEFAULT_ACCOUNT"]),
--- cdk-jenkins/codebuild_stack/codebuild.py
+++ cdk-jenkins/codebuild_stack/codebuild.py
@@ -26,31 +26,35 @@ class CodeBuildStack(Stack):
         super().__init__(scope, id, **kwargs)
 
         joern_ecr_repository = ecr.Repository.from_repository_name(
-            self, "joern-scanner", "joern-scanner")
+            self, "joern-scanner", "joern-scanner"
+        )
 
         # create s3 bucket the name is jeknins-build-artifacts
         s3_bucket = s3.Bucket(
-            self, "jenkins-build-artifacts", 
+            self,
+            "jenkins-build-artifacts",
             auto_delete_objects=True,
             encryption=s3.BucketEncryption.UNENCRYPTED,
             block_public_access=s3.BlockPublicAccess.BLOCK_ALL,
             enforce_ssl=True,
             versioned=True,
-            removal_policy=cdk.RemovalPolicy.DESTROY
+            removal_policy=cdk.RemovalPolicy.DESTROY,
         )
 
         # CodeBuild project that builds the webgoat jar
         codebuild_jar = codebuild.Project(
             self,
             "BuildImage",
-            build_spec=codebuild.BuildSpec.from_asset("codebuild_webgoat_buildspec.yaml"),
+            build_spec=codebuild.BuildSpec.from_asset(
+                "codebuild_webgoat_buildspec.yaml"
+            ),
             source=codebuild.Source.git_hub(owner="WebGoat", repo="WebGoat"),
-            #artifacts=codebuild.Artifacts.s3(bucket=s3_bucket,package_zip=True,encryption=False),
-            artifacts=codebuild.Artifacts.s3(bucket=s3_bucket,encryption=False),
+            # artifacts=codebuild.Artifacts.s3(bucket=s3_bucket,package_zip=True,encryption=False),
+            artifacts=codebuild.Artifacts.s3(bucket=s3_bucket, encryption=False),
             environment=codebuild.BuildEnvironment(
                 privileged=True,
                 compute_type=codebuild.ComputeType.MEDIUM,
-                build_image=codebuild.LinuxBuildImage.AMAZON_LINUX_2_5
+                build_image=codebuild.LinuxBuildImage.AMAZON_LINUX_2_5,
             ),
             environment_variables={
                 "AWS_ACCOUNT_ID": codebuild.BuildEnvironmentVariable(
@@ -67,12 +71,14 @@ class CodeBuildStack(Stack):
             self,
             "JoernScan",
             build_spec=codebuild.BuildSpec.from_asset("codebuild_joern_buildspec.yaml"),
-            #source=codebuild.Source.git_hub(owner="joernio", repo="joern"),
-            source=codebuild.Source.s3(bucket=s3_bucket,path="BuildImage74257FD8-G2bjbCQI8qQK/35/results.zip"),
+            # source=codebuild.Source.git_hub(owner="joernio", repo="joern"),
+            source=codebuild.Source.s3(
+                bucket=s3_bucket, path="BuildImage74257FD8-G2bjbCQI8qQK/35/results.zip"
+            ),
             environment=codebuild.BuildEnvironment(
                 privileged=True,
                 compute_type=codebuild.ComputeType.LARGE,
-                build_image=codebuild.LinuxBuildImage.AMAZON_LINUX_2_5
+                build_image=codebuild.LinuxBuildImage.AMAZON_LINUX_2_5,
             ),
             environment_variables={
                 "AWS_ACCOUNT_ID": codebuild.BuildEnvironmentVariable(
@@ -84,6 +90,22 @@ class CodeBuildStack(Stack):
             },
         )
 
+        cdk_build = codebuild.Project(
+            self,
+            "WebCoatBuild",
+            build_spec=codebuild.BuildSpec.from_asset(
+                "codebuild_webgoat_stack_buildspec.yaml"
+            ),
+            source=codebuild.Source.git_hub(
+                owner="yiuc", repo="devsecops-jenkins-scanner"
+            ),
+            environment=codebuild.BuildEnvironment(
+                build_image=codebuild.LinuxBuildImage.AMAZON_LINUX_2_5,
+                compute_type=codebuild.ComputeType.MEDIUM,
+                privileged=True,
+            ),
+        )
+
         joern_ecr_repository.grant_pull(codebuild_joern)
 
         # cfn_codebuild = build_jar.node.default_child
@@ -93,9 +115,5 @@ class CodeBuildStack(Stack):
         s3_bucket.grant_read_write(codebuild_jar)
         s3_bucket.grant_read_write(codebuild_joern)
 
-        CfnOutput(
-                self, "BuildProjectName", value=codebuild_jar.project_name
-        )
-        CfnOutput(
-                self, "JoernScanProjectName", value=codebuild_joern.project_name
-        )
+        CfnOutput(self, "BuildProjectName", value=codebuild_jar.project_name)
+        CfnOutput(self, "JoernScanProjectName", value=codebuild_joern.project_name)
